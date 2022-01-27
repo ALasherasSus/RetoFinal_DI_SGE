e3ab24ea6391bf5d648131a1f31062bba9bfc60e
ALasherasSus <75242448+ALasherasSus@users.noreply.github.com>
2022-01-27 09:24:02 +0100

    Modificar completado

--- ProyectoSGE/ModificarRegistros.aspx.vb
+++ ProyectoSGE/ModificarRegistros.aspx.vb
@@ -23,8 +23,29 @@ Public Class ModificarRegistros
             Me.GridView1.DataBind()
 
             TextBoxProductName.Text = das1.Tables(0).Rows(0)(1).ToString
+            TextBoxQuantityPerUnit.Text = das1.Tables(0).Rows(0)(2).ToString
+            TextBoxUnitPrice.Text = das1.Tables(0).Rows(0)(3).ToString
         Catch ex As Exception
             'Me.LabelContador.Text = ex.Message
         End Try
+    End Sub
+
+    Protected Sub ButtonModificar_Click(sender As Object, e As EventArgs) Handles ButtonModificar.Click
+        Dim stringconexion As String =
+            ConfigurationManager.ConnectionStrings("ELORRIETA").ConnectionString
+        Dim conexion As New OleDbConnection(stringconexion)
+
+        If (TextBoxProductName.Text.Length > 0 And TextBoxQuantityPerUnit.Text.Length > 0 And TextBoxUnitPrice.Text.Length > 0 And IsNumeric(TextBoxBuscar.Text)) Then
+            Try
+                conexion.Open()
+                Dim SQL As String = "UPDATE Products SET ProductName='" & TextBoxProductName.Text & "', QuantityPerUnit='" & TextBoxQuantityPerUnit.Text & "', UnitPrice=" & TextBoxUnitPrice.Text & " WHERE ProductID=" & TextBoxBuscar.Text & ";"
+                Dim command As New OleDbCommand(SQL, conexion)
+                Dim contador As Double = command.ExecuteNonQuery
+
+                MsgBox("Fila actualizada correctamente.")
+            Catch ex As Exception
+                MsgBox("Fallo al actualizar la fila. Compruebe los campos.")
+            End Try
+        End If
     End Sub
 End Class
